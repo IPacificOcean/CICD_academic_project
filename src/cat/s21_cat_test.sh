92f026b92cd8be320ad06555eef526ee8c194368
qyburnbo <qyburnbo@student.21-school.ru>
2023-02-20 02:07:43 +0300

    test

--- src/cat/s21_cat_test.sh
+++ src/cat/s21_cat_test.sh
@@ -10,7 +10,7 @@ for var in -b -e -n -s -t -v #-E -T --number-nonblank --number --squeeze-blank
 do
           TEST1="$var $TEST_FILE"
           echo "$TEST1"
-          ./s21_cat $TEST1 > s21_cat.txt
+          ./compiled-code/s21_cat $TEST1 > s21_cat.txt
           cat $TEST1 > cat.txt
           DIFF_RES="$(diff -s s21_cat.txt cat.txt)"
           if [ "$DIFF_RES" == "Files s21_cat.txt and cat.txt are identical" ]
