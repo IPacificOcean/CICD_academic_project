860517090e93a1bea87a16942d6c69bd3a1d066f
qyburnbo <qyburnbo@student.21-school.ru>
2023-02-22 02:33:00 +0300

    test deploy

--- src/transfer.sh
+++ src/transfer.sh
@@ -2,4 +2,4 @@ cd cat/compiled-code
 scp s21_cat deploy@172.24.116.8:~
 cd ../../grep/compiled-code
 scp s21_grep deploy@172.24.116.8:~
-ssh deploy@172.24.116.8 sudo mv  * /usr/local/bin 
+ssh deploy@172.24.116.8 sudo mv  s21_cat s21_grep  /usr/local/bin 
